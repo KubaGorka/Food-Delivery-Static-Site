0bc7d2b8e203e0476395ef014b43b5ea4f5fb885
Kuba <kubagorka14@gmail.com>
2021-03-22 13:37:33 +0100

    added hiding navbar

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ AOS.init({
   duration: 1000,
   once: true,
 });
+// open/close menu on mobile
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".menu");
 let menuItems = document.querySelectorAll(".menu-item");
@@ -15,3 +16,11 @@ menuItems.forEach((item) => {
     menu.classList.toggle("menu-hidden");
   });
 });
+// hide menu on scroll
+let nav = document.querySelector(".nav");
+let prev = window.pageYOffset;
+window.onscroll = () => {
+  let curr = window.pageYOffset;
+  prev > curr ? (nav.style.top = "0") : (nav.style.top = "-100%");
+  prev = curr;
+};
